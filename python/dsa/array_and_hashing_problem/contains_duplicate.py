9e1a057b7ba1bf14d1bedd73d32ec9034b05a207
NIshant <nishu.saxena@gmail.com>
2024-04-04 12:55:50 -0400

    Add comment and complexity to the contain duplicate code

--- python/dsa/array_and_hashing_problem/contains_duplicate.py
+++ python/dsa/array_and_hashing_problem/contains_duplicate.py
@@ -4,10 +4,21 @@ and return false if every element is distinct.
 """
 
 def is_contains_dulicate(arr):
-    temp_arr=[]
+    """
+    Check if an integer array contains any duplicate values.
+    
+    Time Complexity: O(n)
+    - n is the number of elements in the input array 'arr'.
+    - In the worst case, the function may need to iterate through all elements to find duplicates.
+
+    Memory Complexity: O(n)
+    - Uses a temporary list 'temp_arr' to store unique elements.
+    - The size of 'temp_arr' can grow up to the size of 'arr' in the worst case if 'arr' has no duplicates.
+    """
+    temp_arr = []
     for elem in arr:
-        if elem not in temp_arr:
-            temp_arr.append(elem)
+        if elem not in temp_arr:  # Checking if the element is already in the list
+            temp_arr.append(elem)  # Adding the element to the list if it's not already there
         else:
-            return True
-    return False
+            return True  # Returning True if a duplicate is found
+    return False  # Returning False if no duplicates are found
